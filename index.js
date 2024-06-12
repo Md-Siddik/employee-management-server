325cde52138c4380da2570513387739f1e0f1408
unknown <abusiddick84@gmail.com>
2024-06-12 19:40:31 +0600

    Sarary date and month

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -25,6 +25,46 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    const employeeCollection = client.db('EM-Espial').collection('User');
+
+    app.post('/register', async (req, res) => {
+      const employee = req.body;
+      console.log(employee);
+      const result = await employeeCollection.insertOne(employee);
+      res.send(result);
+    })
+
+    app.get('/register', async (req, res) => {
+      const cursor = employeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.patch('/register/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updatedItem = req.body;
+      const item = {
+        $set: {
+          verified: updatedItem.verified,
+        }
+      }
+      
+      const result = await employeeCollection.updateOne(filter, item);
+      res.send(result);
+    })
+
+    app.get('/register/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)};
+      const result = await employeeCollection.findOne(query);
+      res.send(result);
+      
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -36,9 +76,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('em is espiling')
+  res.send('em is espiling')
 })
 
 app.listen(port, () => {
-    console.log(`em-Espial server is running on port ${port}`)
+  console.log(`em-Espial server is running on port ${port}`)
 })
